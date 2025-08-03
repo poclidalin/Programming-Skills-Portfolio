74df0cf2c3607cdb3f1fcfdbb472a0e51931772e
poclidalin <poclidalin@yahoo.com>
2025-08-03 23:19:35 +0100

    Add files via upload
    I declared 3 variables in order to store age,name and hometown using appropriate data types. Printed each on a new line using a single cout statement, as required by the task.

---
+++ 03.Biography/Biography.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Declare the variable age
+    int age = 36;
+    
+    // Declare the variable name
+    string name = "Alin";
+    
+    // Declare the variable hometown
+    string hometown = "Timisoara";
+    
+    // Display name, age and hometown, each on a new line
+    cout << name << endl << age << endl << hometown;
+
+    // End the program
+    return 0;
+
+}
