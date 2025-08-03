03c282d17003f8c221601289db0a51f1add94338
poclidalin <poclidalin@yahoo.com>
2025-08-03 23:15:32 +0100

    Add files via upload
    In this exercise, I declared two variables set to 8 and 10, added them together, stored the result in a third variable and displayed the result using cout.

---
+++ 02.SimpleSums/SimpleSums.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Set a number to 8
+    int x = 8;
+    
+    // Set a second number to 10
+    int y = 10;
+
+    // Find out the sum of the two numbers
+    int z = x + y;
+
+    // Display the result
+    cout << z;
+
+    // end the program
+    return 0;
+}
