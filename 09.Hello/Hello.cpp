9dfa540a49de10939578680d0b699f3d28969b2f
poclidalin <poclidalin@yahoo.com>
2025-08-04 02:25:24 +0100

    Add files via upload
    Filled in the blanks to make a program that displays "Hello". Created and called a function to show the message on the screen.

---
+++ 09.Hello/Hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Define function that display "Hello"
+void hello() {
+  cout << "Hello" << endl;
+}
+
+int main() {
+
+  // Call function
+  hello();
+
+  // End of program
+  return 0;
+}
